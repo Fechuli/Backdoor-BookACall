76f5a9e277e61c16a13ca7531483b5c35a6639b7
Federico Fiaschi <federicofiaschi277@gmail.com>
2024-07-19 10:44:39 +0200

    admin-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import ClientForm from "@/components/forms/ClientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image alt="Backdoor's logo" src={"/icons/logo.png"} height={1000} width={1000} priority={true} className="mb-18 h-40 w-fit -translate-x-14"/>
